fd78e3021cfd7ea74515f5b0d6f76c6b90b00eab
Nauval <sigantengnoval@gmail.com>
2025-03-26 23:15:08 +0700

    :lock: Private image url variable (#13)

--- lib/features/trip/pages/trip_detail_page.dart
+++ lib/features/trip/pages/trip_detail_page.dart
@@ -7,7 +7,7 @@ class TripDetailPage extends StatelessWidget {
 
   const TripDetailPage({super.key, required this.id});
 
-  static const List<String> imageUrls = [
+  static const List<String> _imageUrls = [
     'https://picsum.photos/800/400',
     'https://picsum.photos/801/401',
     'https://picsum.photos/802/402',
@@ -132,14 +132,14 @@ class TripDetailPage extends StatelessWidget {
       height: 250,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: imageUrls.length,
+        itemCount: _imageUrls.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 4.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Image.network(
-                imageUrls[index],
+                _imageUrls[index],
                 fit: BoxFit.cover,
                 width: MediaQuery.of(context).size.width * 0.8,
                 loadingBuilder: (context, child, loadingProgress) {
