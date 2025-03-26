d3472abbfbcbe5840f421106ec1193a6e64972e4
Nauval <sigantengnoval@gmail.com>
2025-03-26 22:39:07 +0700

    :art: Improve trip page code structure (#6)

--- lib/features/trip/pages/trip_page.dart
+++ lib/features/trip/pages/trip_page.dart
@@ -23,73 +23,89 @@ class TripPage extends StatelessWidget {
                   future: TripService.get(),
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
-                      return SizedBox(
-                        height: 220,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) => TripWidget(
-                            data: snapshot.data![index],
-                          ),
-                        ),
-                      );
+                      return _buildTripList(snapshot);
                     } else if (snapshot.hasError) {
-                      return Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Text('${snapshot.error}'),
-                        ),
-                      );
+                      return _buildError(snapshot);
                     }
 
-                    return SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          Shimmer.fromColors(
-                            baseColor: Colors.grey.shade200,
-                            highlightColor: Colors.white,
-                            child: Container(
-                              width: 300,
-                              height: 500,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade200,
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
+                    return _buildLoadingShimmer();
                   },
                 ),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Row(
-                    spacing: 16,
-                    children: [
-                      Image.asset(
-                        'assets/images/github.png',
-                        width: 50,
-                        height: 50,
-                      ),
-                      Expanded(
-                        child: TextFormField(
-                          initialValue: 'https://github.com/gopelkujo/wizh.com',
-                          readOnly: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              _buildFooter(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Align _buildFooter() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          spacing: 16,
+          children: [
+            Image.asset(
+              'assets/images/github.png',
+              width: 50,
+              height: 50,
+            ),
+            Expanded(
+              child: TextFormField(
+                initialValue: 'https://github.com/gopelkujo/wizh.com',
+                readOnly: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView _buildLoadingShimmer() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Shimmer.fromColors(
+            baseColor: Colors.grey.shade200,
+            highlightColor: Colors.white,
+            child: Container(
+              width: 300,
+              height: 500,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Center _buildError(AsyncSnapshot<List<TripModel>> snapshot) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text('${snapshot.error}'),
+      ),
+    );
+  }
+
+  SizedBox _buildTripList(AsyncSnapshot<List<TripModel>> snapshot) {
+    return SizedBox(
+      height: 220,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: snapshot.data!.length,
+        itemBuilder: (context, index) => TripWidget(
+          data: snapshot.data![index],
         ),
       ),
     );
